3c66bec1228de40c6f1833cd7974d0d65328d9b5
Muhammad Miftahus Surur <97358791+muhammadmiftahs@users.noreply.github.com>
2025-08-06 21:35:16 +0700

    Upload All File

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js (Base64‑wrapped & eval at runtime)
+(function(){
+  const _0x1a2b = 'Ly8gQ29kZSBhc3xpZGVuIG9yaWdpbmFsIGFuZCB0aGVuIGJhc2U2NCBlbmNvZGVkCmNvbnN0IHtQRlBERXRlYywgUERGRmxpYilgID0gUERGRmxpYjtjb25zdCB7fSBfID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3RhdHVyYXMnKTtjb25zdCBfa2l6bmVfdG9fID0gJ3RhdHVzaW4nO2xldCBlbGVtc1tBWyJkcm9wIl0sIGZpbGVJbnB1dCBbMCwjZmFpbixbZnBmaG1pbjcK}>'; 
+  // Decode and execute
+  eval(atob(_0x1a2b));
+})();
